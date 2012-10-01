f4b25314057ee1142559b827e702620ccb97853d
Steve Roettger <steve@steveroettger.com>
2012-10-01 11:59:02 -0400

    testing vote method with 24 hour

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -44,20 +44,20 @@ class ContestantsController < ApplicationController
   
   #method definition for vote feature
   def vote
-    vote = current_user.contestant_votes.new(value: params[:value], contestant_id: params[:id])
+    vote = current_user.contestant_votes.new(value: params[:value], contestant_id: params[:id])    
+    if current_user.contestant_votes == nil
+      vote.save
+      redirect_to :back, notice: "Success!"
+    elsif current_user.contestant_votes != nil && ((Time.now - (current_user.contestant_votes.last.created_at)) / 3600) <= 24
+      redirect_to :back, alert: "Unable to vote, only vote per day."
+    else
+      redirect_to :back, alert: "Unable to vote."
+   end
+  end
+end
+
 #    if ((today - last_vote) / 3600) <= 24
 #      redirect_to :back, alert: "Unable to vote, only vote per day."
 #    else vote.save
 #      redirect_to :back, notice: "Success!"
 #    end
-    
-#    if current_user.contestant_votes == nil
-      vote.save
-      redirect_to :back, notice: "Success!"
-#    elsif current_user.contestant_votes != nil && ((Time.now - (current_user.contestant_votes.last.created_at)) / 3600) <= 24
-#      redirect_to :back, alert: "Unable to vote, only vote per day."
-#    else
-#      redirect_to :back, alert: "Unable to vote."
-#   end
-  end
-end
