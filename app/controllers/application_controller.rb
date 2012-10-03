8d1bd6191d7446bd0d6c657d89ae3c8b0de46dae
Steve Roettger <steve@steveroettger.com>
2012-10-03 19:56:31 -0400

    added p3p gem to fix IE/Safari loop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :set_p3p
+  
+  # Headers for Internet Explorer
+      #   required so that IE accepts the cookies and prevents a redirect-loop
+      #
+  before_filter :ie_headers
+    def ie_headers
+      response.headers['P3P'] = "CP=\"CAO PSA OUR\""
+    end
+  
   private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
-  
-  # for IE session cookies thru iframe  
-  def set_p3p  
-      headers['P3P'] = 'CP="ALL DSP COR CURa ADMa DEVa OUR IND COM NAV"'  
-  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ end
 
 module FacebookApp
   class Application < Rails::Application
+    config.middleware.insert_before ActionDispatch::Session::CookieStore, Rack::P3p
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
