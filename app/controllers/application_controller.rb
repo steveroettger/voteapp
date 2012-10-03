6bb571074f9b38818c9e0bec535980ee4c5808a6
Steve Roettger <steve@steveroettger.com>
2012-10-03 19:03:54 -0400

    added p3p header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+  before_filter :set_p3p
   private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+  
+  # for IE session cookies thru iframe  
+  def set_p3p  
+      headers['P3P'] = 'CP="ALL DSP COR CURa ADMa DEVa OUR IND COM NAV"'  
+  end
 end
