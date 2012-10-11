d6ab48487a76b0c045594a1ee716680fa35abc41
Steve Roettger <steve@steveroettger.com>
2012-10-11 13:36:22 -0400

    fix

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -10,20 +10,20 @@ class Contestant < ActiveRecord::Base
   def car_position
     #total track = 480px
     #to make car start at beginning of track :: 480px - 43px (car height) = 437 px
-    #if (437 - contestant_votes.sum(:value)) <= 0
-    #  car_position = 50
-    #else
-    #  car_position = 437 - contestant_votes.sum(:value)
-    #end
-    if contestant_votes.sum(:value) >= 150
-      updated_votes = (contestant_votes.sum(:value) * .70)
-      if (437 - (updated_votes) <= 50
-        car_position = 50
-      else
-        car_position = (437 - (updated_votes))
-      end
+    if (437 - contestant_votes.sum(:value)) <= 0
+      car_position = 50
     else
-      car_position = (437 - contestant_votes.sum(:value))
+      car_position = 437 - contestant_votes.sum(:value)
     end
+    #if contestant_votes.sum(:value) >= 150
+    #  updated_votes = (contestant_votes.sum(:value) * .70)
+    #  if (437 - (updated_votes) <= 50
+    #    car_position = 50
+    #  else
+    #    car_position = (437 - (updated_votes))
+    #  end
+    #else
+    #  car_position = (437 - contestant_votes.sum(:value))
+    #end
   end
 end
